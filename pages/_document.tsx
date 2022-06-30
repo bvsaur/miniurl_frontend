6deb02ca7b694c8d025426cce64afac178d4b6ea
Bruno <brunoveranoc@gmail.com>
2022-06-29 20:12:28 -0500

    adding meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,16 @@ const Document = () => {
           href="https://fonts.googleapis.com/css2?family=Kanit:wght@100;400;700&family=Mukta+Vaani:wght@300;700&display=swap"
           rel="stylesheet"
         />
+        <meta property="og:title" content="MiniURL" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dnvdrksxu/image/upload/v1656551458/MiniURL/MiniURL_Logo_wroegy.png"
+        />
+        <meta property="og:description" content="The best URL Minifier App" />
+        <meta property="og:url" content="https://mini-url.xyz" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
+        <meta property="og:type" content="website" />
       </Head>
       <body>
         <Main />
